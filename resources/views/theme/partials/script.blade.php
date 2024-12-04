efc64c70d47debe11094f3996f4531518e8f0437
Mena Ehab <commanderyt123@gmail.com>
2024-12-04 20:09:18 +0200

    Fix carousel bug

--- resources/views/theme/partials/script.blade.php
+++ resources/views/theme/partials/script.blade.php
@@ -48,9 +48,5 @@
 <script src="{{ asset('theme') }}/lib/waypoints/waypoints.min.js"></script>
 <script src="{{ asset('theme') }}/lib/owlcarousel/owl.carousel.min.js"></script>
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
 <!-- Template Javascript -->
 <script src="{{ asset('theme') }}/js/main.js"></script>
